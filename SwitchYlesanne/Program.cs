1dd3f12b50138794a9a4be64aa7b4f1104c046b6
Elza-R <erutkovs@gmail.com>
2024-01-31 19:28:12 +0200

    add week3 test code

---
+++ SwitchYlesanne/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Palun sisestage oma hinne:");
+
+
+string grade = Convert.ToString(Console.ReadLine().ToUpper());
+
+switch (grade)
+{
+    case "A":
+        Console.WriteLine("Suurepärane!");
+        break;
+    case "B":
+        Console.WriteLine("Väga Hea!");
+        break;
+    case "C":
+        Console.WriteLine("Hea!");
+        break;
+    case "D":
+        Console.WriteLine("Rahuldav");
+        break;
+    case "E":
+        Console.WriteLine("Kasin");
+        break;
+    case "F":
+        Console.WriteLine("Puudulik");
+        break;
+    default:
+        Console.WriteLine("vale väärtus");
+        break;
+}
